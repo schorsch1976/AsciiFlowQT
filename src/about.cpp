9197be45d5212d39ffe7d4cbf5566b75d44b65d7
Georg Gast <georg@schorsch-tech.de>
2019-06-21 16:05:38 +0200

    Fix AboutDlg compilation on Windows

--- src/about.cpp
+++ src/about.cpp
@@ -3,6 +3,8 @@
 
 #include <QDebug>
 #include <QDesktopServices>
+#include <QRegularExpression>
+#include <QUrl>
 
 #define STRINGIFY(string) LOCALSTRING(string)
 #define LOCALSTRING(string) #string
@@ -13,34 +15,41 @@ About::About(QWidget *parent) : QDialog(parent), ui(new Ui::About)
 
 	connect(ui->btnOk, &QPushButton::clicked, [this]() { this->close(); });
 
+    auto open_link = [](QString link) {
+        QRegularExpression ex("&");
+        link.remove(ex);
+		bool res = QDesktopServices::openUrl(QUrl(link));
+		if (!res)
+		{
+			qDebug() << "Failed to open url: " << link;
+		}
+	};
+
 	// Ascii flow
 	ui->labelAsciiFlowVersion->setText(STRINGIFY(GIT_COMMIT_HASH));
 	ui->labelAsciiFlowBranch->setText(STRINGIFY(GIT_BRANCH));
 	ui->labelAsciiFlowDescription->setText(STRINGIFY(GIT_DESCRIPTION));
 
 	connect(ui->commandLinkButtonHomeAsciiFlow, &QCommandLinkButton::pressed,
-			[this]() {
-				QString link = ui->commandLinkButtonHomeAsciiFlow->text();
-				link.remove(0, 1);
-				bool res = QDesktopServices::openUrl(QUrl(link));
-				if (!res)
-				{
-					qDebug() << "Failed to open url: " << link;
-				}
+			[this, open_link]() {
+				open_link(ui->commandLinkButtonHomeAsciiFlow->text());
+			});
+	connect(ui->commandLinkButtonAsciiFlowLicence, &QCommandLinkButton::pressed,
+			[this, open_link]() {
+				open_link(ui->commandLinkButtonAsciiFlowLicence->text());
 			});
 
 	// QT
 	ui->labelRunningQTVersion->setText(qVersion());
 	ui->labelCompiledQTVersion->setText(QT_VERSION_STR);
+
 	connect(ui->commandLinkButtonHomeQT, &QCommandLinkButton::pressed,
-			[this]() {
-				QString link = ui->commandLinkButtonHomeQT->text();
-				link.remove(0, 1);
-				bool res = QDesktopServices::openUrl(QUrl(link));
-				if (!res)
-				{
-					qDebug() << "Failed to open url: " << link;
-				}
+			[this, open_link]() {
+				open_link(ui->commandLinkButtonHomeQT->text());
+			});
+	connect(ui->commandLinkButtonQTLicence, &QCommandLinkButton::pressed,
+			[this, open_link]() {
+				open_link(ui->commandLinkButtonQTLicence->text());
 			});
 }
 
